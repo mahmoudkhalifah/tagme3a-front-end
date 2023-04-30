00f68048ee5688bd6d15232c936762abd557735a
Asmaa Gamal <gasmaa083@gmail.com>
2023-04-30 18:43:08 +0300

    EditPC

--- src/app/Components/AdminComponents/PCManager/edit-prdpc/edit-prdpc.component.ts
+++ src/app/Components/AdminComponents/PCManager/edit-prdpc/edit-prdpc.component.ts
@@ -45,7 +45,7 @@ export class EditPrdpcComponent implements OnInit {
 
     this.myService.updateprdpc(this.pc.id , prd).subscribe({
       next:()=>{
-        this.router.navigate(["admin/PCManager/view-pc"]);
+        //this.router.navigate(["admin/PCManager/view-pc"]);
 
       },
       error:(err)=>{
@@ -54,14 +54,14 @@ export class EditPrdpcComponent implements OnInit {
     });
   }
 
-
-  DeleteProduct(id:any){
+  DeleteProduct(id:any , i :number){
     let prdPc = new PrdPcDeleteDTO(id);
     console.log(prdPc);
     console.log(this.pc.id);
     this.myService.deletePrdPC(this.pc.id , prdPc).subscribe({
       next:()=>{
-        this.getPcById();
+        this.pc.products.splice(i , 1);
+
       },
       error:(err)=>{
         console.log(err);
