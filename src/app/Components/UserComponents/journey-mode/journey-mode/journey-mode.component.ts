209e5e5d14ccbbf1834f18a630f4ae57dc5a7652
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2023-04-30 15:26:53 +0300

    journey mode without price handling and good frontend

--- src/app/Components/UserComponents/journey-mode/journey-mode/journey-mode.component.ts
+++ src/app/Components/UserComponents/journey-mode/journey-mode/journey-mode.component.ts
@@ -1,54 +1,75 @@
-import { Component } from '@angular/core';
+import { JourneyModeService } from './../../../../services/journey-mode.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-journey-mode',
   templateUrl: './journey-mode.component.html',
   styleUrls: ['./journey-mode.component.css']
 })
-export class JourneyModeComponent {
-  steps = [
-    {
-      categoryId:1,
-      name:"Motherboard",
-      visited:true,
-    },
-    {
-      categoryId:2,
-      name:"CPU",
-      visited:true,
-    },
-    {
-      categoryId:3,
-      name:"GPU",
-      visited:true,
-    },
-    {
-      categoryId:4,
-      name:"RAM",
-      visited:false,
-    },
-    {
-      categoryId:5,
-      name:"Hard desk",
-      visited:false,
-    },
-    {
-      categoryId:6,
-      name:"Cooler",
-      visited:false,
-    },
-    {
-      categoryId:7,
-      name:"Case",
-      visited:false,
-    },
-  ]
+export class JourneyModeComponent implements OnInit {
+
+  constructor(private journeyModeService:JourneyModeService) {
+
+  }
+  ngOnInit(): void {
+    this.journeyModeService.getCategories().subscribe({
+      next: (data)=>{
+        this.steps = data;
+        this.steps.forEach((s)=>s.visited=false);
+        this.steps.unshift({
+          name:"Budget",
+          visited:true,
+        });
+        console.log(this.steps);
+      },
+      error: (err)=>{
+        console.log(err);
+      }
+    })
+  }
+  totalPrice:number=0;
+  //remainingPrice:number=0;
+  steps:any[] = [];
+  selectedProducts:any[]=[];
+  products:any[]=[]
+
 
   selectedCategory = 0;
 
   changeStepInfo(index:number) {
     this.selectedCategory = index;
-    console.log("hi")
-    console.log(index);
+  }
+  getProducts(maxPrice:number) {
+    this.journeyModeService.getProducts(this.steps[this.selectedCategory].categoryId,maxPrice)
+    .subscribe({
+      next:(data)=>{
+        this.products=data;
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    });
+  }
+
+  selectProduct(prd:any,quantity:number) {
+    // this.remainingPrice-=prd.price;
+    // this.remainingPrice+=prd.discount;
+    if(this.selectedCategory-1<this.selectProduct.length)
+      this.selectedProducts[this.selectedCategory-1]={product:prd,quantity};
+    else this.selectedProducts.push({product:prd,quantity});
+    this.nextStep();
+  }
+
+  nextStep() {
+    this.selectedCategory++;
+    this.steps[this.selectedCategory].visited = true;
+    this.products = [];
+  }
+
+  get remainingPrice() {
+    //could decrease it after every time purchanse
+    let remaining = this.totalPrice;
+    this.selectedProducts.forEach((prd:any)=>remaining-=(prd.product.price-prd.product.discount)*prd.quantity);
+    return remaining;
   }
 }
