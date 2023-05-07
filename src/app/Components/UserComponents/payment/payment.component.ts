e131dfe1107d0b67f5e02dbe0a6b76124fa11e1e
Mariam Hesham <mariamhesham503@gmail.com>
2023-05-07 12:55:57 +0200

    IntegratePaymentWithCheckout

--- src/app/Components/UserComponents/payment/payment.component.ts
+++ src/app/Components/UserComponents/payment/payment.component.ts
@@ -9,35 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class PaymentComponent implements OnInit {
   handler:any = null;
   constructor(private http: HttpClient) { }
-  ngOnInit(): void {
-  //  this.loadStripe();
-  }
-  pay(amount:any) {
-console.log(amount=1000);
+  ngOnInit() {
 
-  this.http.post<{ clientSecret: string }>('https://localhost:7004/api/Payment/create-payment-intent', amount)
+    this.loadStripe();
+  }
+
+  pay(amount:any) {
+amount = 1000;
+console.log(amount);
+this.http.post<{ clientSecret: string }>('https://localhost:7004/api/Payment/create-payment-intent', amount)
     .subscribe(data => {
     var handler = (<any>window).StripeCheckout.configure({
       key: 'pk_test_51N2nJ5BnjdE1DOem7oq1cMTAsCvVVZvzpb1xL8ArfZv2wGUZUDfx8JVbhHzi2NXFNvsfNwS7wp0CGQaNaKPISGkp00rgpk5zE7',
       locale: 'auto',
-      currency:'usd',
       token: function (token: any) {
         // You can access the token ID with `token.id`.
         // Get the token ID to your server-side code for use.
-        console.log("GOWA PAY");
         console.log(token)
         alert('Token Created!!');
       }
     });
 
     handler.open({
-      name: 'Tagme3a',
+      name: 'Demo Site',
       description: '2 widgets',
-      amount: amount * 100,
-      token:data.clientSecret
+      amount: amount * 100
     });
   });
   }
+
   loadStripe() {
 
     if(!window.document.getElementById('stripe-script')) {
@@ -49,13 +49,10 @@ console.log(amount=1000);
         this.handler = (<any>window).StripeCheckout.configure({
           key: 'pk_test_51N2nJ5BnjdE1DOem7oq1cMTAsCvVVZvzpb1xL8ArfZv2wGUZUDfx8JVbhHzi2NXFNvsfNwS7wp0CGQaNaKPISGkp00rgpk5zE7',
           locale: 'auto',
-          currency:'usd',
           token: function (token: any) {
             // You can access the token ID with `token.id`.
             // Get the token ID to your server-side code for use.
-            console.log("GOWA LOAD STRIPE");
             console.log(token)
-
             alert('Payment Success!!');
           }
         });
@@ -63,8 +60,6 @@ console.log(amount=1000);
 
       window.document.body.appendChild(s);
     }
-
   }
-
 
 }
