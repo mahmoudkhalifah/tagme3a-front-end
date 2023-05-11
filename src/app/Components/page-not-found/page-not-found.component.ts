092f613f2616fce65efd098c6c32e67517e9c745
Hassan752 <h_mostafa54@yahoo.com>
2023-05-11 15:55:18 +0300

    Product image updated , pc product ui , page not found auth

--- src/app/Components/page-not-found/page-not-found.component.ts
+++ src/app/Components/page-not-found/page-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserAuthService } from 'src/app/services/user-auth.service';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent {
+
+  constructor(private auth : UserAuthService) {}
+
+  get isAdmin() {
+    return this.auth.isAdmin();
+  }
 
 }
--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  static apiBaseUrl:string = "https://tagme3a-back-endapi20230511001910.azurewebsites.net/api/";
+  static apiBaseUrl:string = "https://localhost:7004/api/";
   static adminRole:string = "Admin";
   static userRole:string = "User";
 }
