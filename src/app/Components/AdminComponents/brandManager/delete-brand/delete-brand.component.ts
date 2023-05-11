67a3fe418e1631d2ed73e9274584f8e82c68e9c4
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2023-05-11 17:08:47 +0300

    fixed something

--- src/app/Components/AdminComponents/brandManager/delete-brand/delete-brand.component.ts
+++ src/app/Components/AdminComponents/brandManager/delete-brand/delete-brand.component.ts
@@ -36,6 +36,7 @@ export class DeleteBrandComponent {
     this.service.deleteBrandById(this.Id).subscribe({
       next:()=>{
         // this.router.navigate(["admin/brandManager/view-brands"])
+        this.router.navigate(["admin/dashboard"])
       },
       error:(err)=>{
         if(err.status == 400) {
